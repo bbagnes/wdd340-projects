8c08557a5fd110dad0bce1c8bafe6d1613886c71
bbagnes <bbagnes@byupathway.edu>
2025-09-13 16:04:42 -0600

    Added req files per weekly assignment, week 2.

---
+++ database/assignment2.sql
@@ -0,0 +1,28 @@
+1. 
+INSERT INTO public.account (account_firstname, account_lastname, 
+account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+2.
+UPDATE public.account SET account_type = 'Admin' WHERE account_id = 1 ;
+
+3.
+DELETE FROM public.account WHERE account_id = 1;
+
+4.
+UPDATE public.inventory SET inv_description =
+REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+5.
+SELECT inv_make, inv_model FROM public.inventory
+JOIN public.classification 
+ON public.inventory.classification_id = public.classification.classification_id
+WHERE public.classification.classification_id = 2;
+
+6.
+UPDATE public.inventory 
+SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
+
+
